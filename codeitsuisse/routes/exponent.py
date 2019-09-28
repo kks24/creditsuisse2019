c503e955ba094d07f2257e56f86b9690e74154da
Kah Seng <kks_24@hotmail.com>
2019-09-28 16:09:39 +0800

    Update exponent.py

--- codeitsuisse/routes/exponent.py
+++ codeitsuisse/routes/exponent.py
@@ -18,15 +18,14 @@ def evaluate_exponent():
     return jsonify(result);
 
 def Exponent(inputValue):
-	trailingList = [[0],[2,4,8,6],[3,9,7,1],[4,6],[5],[6],[7,9,3,1],[8,4,2,6],[9,1]]
+	trailingList = [[0],[1],[2,4,8,6],[3,9,7,1],[4,6],[5],[6],[7,9,3,1],[8,4,2,6],[9,1]]
 
 	n = inputValue["n"]
 	p = inputValue["p"]
 
 	# LAST DIGIT
 	num = n % 10
-	multiple = (p-1) % len(trailingList[num-1])
-	last_digit = trailingList[num-1][multiple]
+	last_digit = options[(p % len(options))]
 
 	# FIRST DIGIT
 	log_start = p * (math.log(n) / math.log(10))
